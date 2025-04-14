fdc2a25567d2feb69385661b892c54b398905754
Yashas-Sagili <yashassagili@gmail.com>
2025-04-15 00:23:27 +0530

    Fixed the app.js file

--- app.js
+++ app.js
@@ -16,9 +16,6 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
-
-
-
 //Intializing the application
 const PORT = process.env.PORT || 5000;
 
