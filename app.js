02a6a86f3d1ef9ff27894589c2646efa57067753
Yashas-Sagili <yashassagili@gmail.com>
2025-04-15 00:22:29 +0530

    Fixed the app.js file

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
-// Connecting to the datbase
-connectToMongo();
+
+
 
 //Intializing the application
 const PORT = process.env.PORT || 5000;
@@ -25,3 +25,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`app listening on port ${PORT}`);
 });
+
+// Connecting to the datbase
+connectToMongo();
